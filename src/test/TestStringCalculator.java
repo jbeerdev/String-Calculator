95fb96c7d70b4c9bc65ad50c3900cad811c5178f
jbeerdev <jbeerbros@gmail.com>
2011-01-04 17:02:48 +0100

    First Kata commit

---
+++ src/test/TestStringCalculator.java
@@ -0,0 +1,29 @@
+package test;
+
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class TestStringCalculator extends TestCase{
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	public void test_addNoneNumbers(){
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
